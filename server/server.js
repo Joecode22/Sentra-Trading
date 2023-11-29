2cb86fa6cc1179c973352bc41f5295410b4ae86a
Joecode22 <joe.code.mckinney@gmail.com>
2023-11-28 20:31:46 -0600

    set up basics for server.js file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 //import express and initialize it
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 //Use Express middleware for parsing JSON and handling URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/SentraTrading', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+// Define routes here
+// Example: app.use('/api/users', usersRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
